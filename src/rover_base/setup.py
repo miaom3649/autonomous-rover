fdf71edda0ec9c9e368e2a45b584c4e52bd456f7
Michael Miao <miaom3649@gmail.com>
2026-05-25 16:41:36 -0400

    Add files for position estimation based on Kalman filter

---
+++ src/rover_base/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = "rover_base"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Michael Miao",
+    maintainer_email="miaom3649@gmail.com",
+    description="Hardware abstraction layer for the autonomous rover.",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "ultrasonic_sensor_node = rover_base.ultrasonic_sensor_node:main",
+        ],
+    },
+)
---
+++ src/rover_navigation/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = "rover_navigation"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Michael Miao",
+    maintainer_email="miaom3649@gmail.com",
+    description="Navigation stack for the autonomous rover.",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "position_estimator_node = rover_navigation.position_estimator_node:main",
+        ],
+    },
+)
