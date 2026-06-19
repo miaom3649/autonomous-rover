9b26665937764df657c7ba0699dd98bea3f28808
Michael Miao <miaom3649@gmail.com>
2026-06-19 13:56:45 -0400

    Add calibration values.

--- src/rover_base/rover_base/drive_node.py
+++ src/rover_base/rover_base/drive_node.py
@@ -37,8 +37,8 @@ class DriveNode(Node):
         if not self._use_sim:
             from picarx import Picarx  # type: ignore[import]
             self._px = Picarx()
-            self._px.set_cam_tilt_angle(0)
-            self._px.set_cam_pan_angle(0)
+            self._px.set_cam_tilt_angle(7)
+            self._px.set_cam_pan_angle(13)
         else:
             self._px = None
 
